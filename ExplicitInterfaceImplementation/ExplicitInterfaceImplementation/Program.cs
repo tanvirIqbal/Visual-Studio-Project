c03f0ff3caccb72c4b5ebe9ad7857597236a242c
tanviriqbal <tanviriqbal@cel.local>
2017-01-23 12:09:58 +0600

    Explicit interface implementation in C#

---
+++ ExplicitInterfaceImplementation/ExplicitInterfaceImplementation/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExplicitInterfaceImplementation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            I1 iObj1 = new A();
+            iObj1.Method1();
+
+            I2 iObj2 = new A();
+            iObj2.Method1();
+
+            Console.ReadLine();
+        }
+    }
+    interface I1
+    {
+        void Method1();
+    }
+    interface I2
+    {
+        void Method1();
+    }
+    class A : I1, I2
+    {
+        void I1.Method1()
+        {
+            Console.WriteLine("I1");
+        }
+        void I2.Method1()
+        {
+            Console.WriteLine("I2");
+        }
+    }
+}
