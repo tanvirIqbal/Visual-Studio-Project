9b2f723412d3cb191a3327abde2f5ad1fec78539
tanviriqbal <tanviriqbal@cel.local>
2017-01-23 12:10:58 +0600

    Static in C#

---
+++ StaticPractice/Masters/CountryMaster.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Utility;
+
+namespace Masters
+{
+    public class CountryMaster
+    {
+        public string CountryCode { get; set; }
+        public string CountryName { get; set; }
+        public string ComputerName
+        {
+            get
+            {
+                return CommonRoutine.GetComputerName();
+            }
+        }
+        public void Insert()
+        {
+            //Record country info into the db
+            CommonRoutine.count++;
+            if (!CommonRoutine.IsEmpty(CountryCode) && !CommonRoutine.IsEmpty(CountryName))
+            {
+
+            }
+        }
+    }
+}
---
+++ StaticPractice/Utility/CommonRoutine.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Utility
+{
+    public static class CommonRoutine
+    {
+        public static int count = 0;
+        public static bool IsEmpty(string value)
+        {
+            if (string.IsNullOrEmpty(value))
+            {
+                return true;
+            }
+            return false;
+        }
+        public static string GetComputerName()
+        {
+            return System.Environment.MachineName;
+        }
+    }
+}
