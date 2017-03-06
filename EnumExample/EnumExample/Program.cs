181825fa0ecf03aa31de5e0fbbc7d1f8b02b5ffe
tanviriqbal <tanviriqbal@cel.local>
2017-03-06 16:35:00 +0600

    Enum and StringBuilder Example

---
+++ EnumExample/EnumExample/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnumExample
+{
+    class Program
+    {
+        enum WeekDays
+        {
+            Monday = 0,
+            Tuesday = 1,
+            Wednesday = 2,
+            Thursday = 3,
+            Friday = 4,
+            Saturday = 5,
+            Sunday = 6
+        }
+
+        enum WeekDays2
+        {
+            Monday,
+            Tuesday,
+            Wednesday,
+            Thursday,
+            Friday,
+            Saturday,
+            Sunday
+        }
+
+        enum WeekDays3
+        {
+            Monday = 10,
+            Tuesday,
+            Wednesday,
+            Thursday,
+            Friday,
+            Saturday,
+            Sunday
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(WeekDays.Friday);
+            Console.WriteLine((int)WeekDays.Friday);
+
+            Console.WriteLine(WeekDays2.Friday);
+            Console.WriteLine((int)WeekDays2.Friday);
+
+            Console.WriteLine((int)WeekDays3.Friday);
+
+
+            Console.WriteLine(Enum.GetName(typeof(WeekDays), 4));
+
+            Console.WriteLine("WeekDays constant names:");
+            foreach (string str in Enum.GetNames(typeof(WeekDays)))
+                Console.WriteLine(str);
+
+            Console.WriteLine("Enum.TryParse():");
+            WeekDays wdEnum;
+            Enum.TryParse<WeekDays>("1", out wdEnum);
+            Console.WriteLine(wdEnum);
+
+            Console.Read();
+        }
+    }
+}
+
+/*
+ Points to Remember :
+The enum is a set of named constant.
+The value of enum constants starts from 0. Enum can have value of any valid numeric type.
+String enum is not supported in C#.
+Use of enum makes code more readable and manageable.
+ * http://www.tutorialsteacher.com/csharp/csharp-enum
+ */
---
+++ StringBuilderExample/StringBuilderExample/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringBuilderExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StringBuilder sb1 = new StringBuilder();
+            sb1.Append("Hello World!! from sb1.");
+            string str = sb1.ToString();
+            Console.WriteLine(str);
+
+            //or
+
+            StringBuilder sb2 = new StringBuilder("Hello World!!", 50);
+            StringBuilder sb3 = new StringBuilder(50);
+
+            //or
+
+            StringBuilder sb4 = new StringBuilder("Hello World!!", 50);
+
+            Console.WriteLine(sb4);
+
+            StringBuilder amountMsg = new StringBuilder("Your total amount is ");
+            amountMsg.AppendFormat("{0:C} ", 25);
+            Console.WriteLine(amountMsg);
+
+            sb4.Insert(5, " C#");
+            Console.WriteLine(sb4);
+
+            sb4.Remove(6, 7);
+            Console.WriteLine(sb4);
+
+            sb2.Replace("World", "C#");
+            Console.WriteLine(sb2);
+            Console.ReadLine();
+        }
+    }
+}
+
+/*
+Points to Remember :
+StringBuilder is mutable.
+StringBuilder performs faster than string when appending multiple string values.
+Initialize StringBuilder as class e.g. StringBuilder sb = new StringBuilder()
+Use StringBuilder when you need to append more than three or four strings.
+Use Append() method to add or append strings with StringBuilder.
+Use ToString() method to get the string from StringBuilder.
+ string str = sb.ToString();
+ * http://www.tutorialsteacher.com/csharp/csharp-stringbuilder
+ */
